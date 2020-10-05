4ac195059cd94b4f32c2956e87987fdea887142d
Hyerim.Hwang <rimhoho@gmail.com>
2020-10-05 04:13:26 -0400

    avocado img/data updated

--- public/js/section-3.js
+++ public/js/section-3.js
@@ -29,7 +29,7 @@ const _regions = [
 
 // READ DATA
 Promise.all([
-  d3.csv('data/avocadosUSAMarket.csv'),
+  d3.csv('data/avocados_market.csv'),
   d3.json('https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json'),
   d3.json('https://opendata.arcgis.com/datasets/85d0ca4ea1ca4b9abf0c51b9bd34de2e_0.geojson')
 ]).then(function([csv, us, cities]) {
